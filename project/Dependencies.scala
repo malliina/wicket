47c8c5465ba9cd1c7e04c53d9d2ae755e838ef39
Michael Skogberg <malliina123@gmail.com>
2013-02-08 13:09:29 +0100

    fixed deps so it compiles

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,8 +5,8 @@ import sbt._
  */
 
 object Dependencies {
-  val wicketVersion = "[6.0.0,)"
-  val utilVersion = "0.64-SNAPSHOT"
+  val wiqueryVersion = "6.2.0"
+  val utilVersion = "0.68-SNAPSHOT"
   val util = "com.mle" %% "util" % utilVersion
   val utilActor = "com.mle" %% "util-actor" % utilVersion
   val utilRmi = "com.mle" %% "util-rmi" % utilVersion
@@ -14,8 +14,8 @@ object Dependencies {
   val utilJdbc = "com.mle" %% "util-jdbc" % utilVersion
   val utilWeb = "com.mle" %% "util-web" % "0.66-SNAPSHOT"
   val warDep = "org.mortbay.jetty" % "jetty" % "6.1.22" % "container"
-  val wiQueryCore = "org.odlabs.wiquery" % "wiquery-core" % wicketVersion
-  val wiQueryUi = "org.odlabs.wiquery" % "wiquery-jquery-ui" % wicketVersion
+  val wiQueryCore = "org.odlabs.wiquery" % "wiquery-core" % wiqueryVersion
+  val wiQueryUi = "org.odlabs.wiquery" % "wiquery-jquery-ui" % wiqueryVersion
   val wiQuery = Seq(wiQueryCore, wiQueryUi)
   val scalaTest = "org.scalatest" %% "scalatest" % "1.9.1" % "test"
 }
